74f859cb2fd05e0080e2d7c11afadf171283997b
GOVERNET\reubenellis <rellis@governet.net>
2017-09-28 17:02:04 -0600

    quote button is working

--- app/containers/randomQuote/randomQuoteContainer.js
+++ app/containers/randomQuote/randomQuoteContainer.js
@@ -3,12 +3,17 @@ import PropTypes from 'prop-types';
 import { fetchRandomQuote } from '../../config/quoteAPI';
 import RandomQuote from '../../components/randomQuote/randomQuote.js';
 
+let quotes = [];
+let random = 0;
+let quote = [];
+
 class RandomQuoteContainer extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
             repos: null,
-            content: ''
+            content: '',
+            author: ''
         };
     }
 
@@ -24,8 +29,8 @@ class RandomQuoteContainer extends React.Component {
         return quotes[random];
     }
 
-    htmlParseQuoteContent = (quote) => {
-        let html = quote.content;
+    htmlParseQuoteContent = (information) => {
+        let html = information;
         var div = document.createElement("div");
         div.innerHTML = html;
         return div.innerText;
@@ -33,32 +38,44 @@ class RandomQuoteContainer extends React.Component {
 
     async fetchRandomQuote() {
         try {
-            this.setState({
-                repos: null
-            });
-            const quotes = await fetchRandomQuote();
+            quotes = await fetchRandomQuote();
 
-            const random = this.randomValue(1, 24);
+            random = this.randomValue(1, 24);
 
-            const quote = this.singleQuoteContent(quotes, random);
-            quote.content = this.htmlParseQuoteContent(quote);
+            quote = this.singleQuoteContent(quotes, random);
+
+            if (quote !== null && this.state.repos !== null) {
+                if (quote.Id === this.state.repos.ID) {
+                    quotes = await fetchRandomQuote();
+                    quote = this.singleQuoteContent(quotes, random);
+                }
+            }
+
+            quote.content = this.htmlParseQuoteContent(quote.content);
+
+            quote.author = this.htmlParseQuoteContent(quote.author);
 
             this.setState({
                 repos: quote,
-                content: quote.content
+                content: quote.content,
+                author: quote.author
             })
 
         } catch (error) {
             console.log(`This is the error: ${error}`);
         }
     }
+
+    updateQuote = () => {
+        this.fetchRandomQuote();
+    }
+
     render() {
         return (
             <div>
                 {<RandomQuote quotes={this.state.repos} content={this.state.content} />}
-                <button className="btn btn-primary">
-                    <i className="fa fa-quote-right"
-                        onClick={this.fetchRandomQuote}></i>Get Quote
+                <button className="btn btn-primary"
+                onClick={this.updateQuote}>Get Quote
                 </button>
             </div>
         )
